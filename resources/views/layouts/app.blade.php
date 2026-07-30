5808dc27dbe2040407cab6d9b55385420d0ed2d6
Saumy Anand <samcool3203@gmail.com>
2026-07-30 06:47:30 +0530

    no follow no index added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="noindex, nofollow">
+    <meta name="robots" content="noindex, nofollow, noarchive">
 
     <title>{{ $title ?? 'Advance Orthopaedic & Spine Center | Advanced Orthopaedic & Spine Care' }}</title>
     <meta name="description" content="Advance Orthopaedic & Spine Center - Premier hospital for knee replacement, endoscopic spine surgery, sports medicine, and 24/7 trauma emergency care.">
